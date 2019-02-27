ef66b7a9adc88d7296bf1c0b0385085c77ff609c
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-27 15:56:29 +0900

    whitenoiselib updated

--- WhiteNoise/Tools/MathStd.cs
+++ WhiteNoise/Tools/MathStd.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WhiteNoise.Tools
+﻿namespace WhiteNoise.Tools
 {
     public class MathStd
     {
