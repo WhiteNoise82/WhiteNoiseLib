d1a3151759dcf6ae5c3ae044e9e1ced2e5afdefd
OH SOUNG KWON <crystals8960@gmail.com>
2019-03-05 15:03:56 +0900

    TextCheck Class Addeds

---
+++ WhiteNoise/MsgBox/txt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteNoise.MsgBox
+{
+    class txt
+    {
+        public bool TextCheck(System.Windows.Forms.TextBox txtBox)
+        {
+            if (txtBox.Text != "") return true;
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("텍스트를 입력을 확인하세요!", "알람",
+                    System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+                txtBox.Focus();
+                return false;
+                
+            }
+        }
+    }
+}
